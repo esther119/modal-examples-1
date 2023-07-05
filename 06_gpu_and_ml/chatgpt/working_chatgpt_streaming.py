944043f5eba7a179ba2e6c3875599a59f17c0be9
swimmingcircle119 <lampmaa22@gmail.com>
2023-07-04 22:08:43 -0700

    web endpoint deployment success

--- 06_gpu_and_ml/chatgpt/working_chatgpt_streaming.py
+++ 06_gpu_and_ml/chatgpt/working_chatgpt_streaming.py
@@ -4,33 +4,66 @@
 # This example shows how to stream from the ChatGPT API as the model is generating a completion, instead of
 # waiting for the entire completion to finish. This provides a much better user experience, and is what you
 # get when playing with ChatGPT on [chat.openai.com](https://chat.openai.com/).
-#
-# You can try this out from the command line using the `modal` CLI, or serve the application and use the
-# included web endpoint.
-#
-# ## Imports and Modal application configuration
-#
-# OpenAI's Python client library is the only package dependency we need. We also need an API key.
-# The former is specified in the Modal application's `image` definition, and the latter is attached to the app's
-# stub as a [`modal.Secret`](/docs/guide/secrets).
 
 from modal import Image, Secret, Stub, web_endpoint
 import os
 
-image = Image.debian_slim().pip_install("openai", "pinecone-client~=2.2.2", "langchain", "openai")
+image = Image.debian_slim().pip_install(
+    "langchain",
+    "openai",
+    "tiktoken==0.3.0",
+    "pinecone-client~=2.2.2"
+)
 stub = Stub(
-    name="example-chatgpt-stream",
+    name="example-langchain",
     image=image,
     secrets=[Secret.from_name("my-openai-secret"), Secret.from_name("my-pinecone-api")],
 )
 
-# This is all the code needed to stream answers back from ChatGPT.
-# Not much code to worry about!
-#
-# Because this Python function is decorated with `@stub.function`, it becomes
-# callable as a Modal remote function. But `stream_chat` can still be used as a
-# regular Python function, which becomes important below.
 
+# # pinecone test
+@stub.function()
+def pinecone_docsearch(user_input):
+    import pinecone
+    from langchain.vectorstores import Pinecone
+    from langchain.embeddings.openai import OpenAIEmbeddings
+
+    embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=os.environ['OPENAI_API_KEY'])
+    print("pinecone start testing")
+    pinecone.init(
+    api_key= os.environ["pinecone-api"],
+    environment='asia-southeast1-gcp-free'   
+    )
+    index_name =  'tim-urban-test'
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    print("pinecone end testing")
+    docs = docsearch.similarity_search(user_input)
+    return docs[0].page_content
+    # return docsearch
+
+
+@stub.function()
+def AI_response_messages(user_input: str, store: str) -> str:
+    template ='''
+    Use the following pieces of context from waitbutwhy to answer the question at the end. 
+    If you don't know the answer, just clarify that you are not sure, but this might be how Tim Urban thinks.
+    '''
+    engineered_user_input = f'Write a blog about {user_input} within 300 words like waitbutwhy using "you" in a casual language'
+    from langchain.schema import (
+        AIMessage,
+        HumanMessage,
+        SystemMessage
+    )
+    from langchain.chat_models import ChatOpenAI
+    chat = ChatOpenAI(model_name='gpt-3.5-turbo-16k-0613', openai_api_key=os.environ['OPENAI_API_KEY'])   
+
+    print("engineered_user_input", engineered_user_input)
+    messages = [
+        SystemMessage(content=template+store),
+        HumanMessage(content=engineered_user_input)
+    ]
+    response=chat(messages)
+    return response.content
 
 @stub.function()
 def stream_chat(prompt: str):
@@ -44,30 +77,6 @@ def stream_chat(prompt: str):
         content = chunk["choices"][0].get("delta", {}).get("content")
         if content is not None:
             yield content
-
-
-
-# pinecone test
-# @stub.function(
-#     image=Image.debian_slim().pip_install("pinecone-client~=2.2.2", "langchain", "openai"),
-#     secrets=[Secret.from_name("my-openai-secret"), Secret.from_name("my-pinecone-api")]
-# )
-@stub.function()
-def pinecone_init():
-    import pinecone
-    from langchain.vectorstores import Pinecone
-    from langchain.embeddings.openai import OpenAIEmbeddings
-
-    print("access embedding")
-    embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=os.environ['OPENAI_API_KEY'])
-    print("pinecone start testing")
-    pinecone.init(
-    api_key= os.environ["pinecone-api"],
-    environment='asia-southeast1-gcp-free'   
-    )
-    index_name =  'tim-urban-test'
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-    print("pinecone end testing")
 
 
 # ## Streaming web endpoint
@@ -85,12 +94,12 @@ def pinecone_init():
 # running in a separate container, sending its results back to the caller over the network.
 
 
-@stub.function()
-@web_endpoint()
-def web(prompt: str):
-    from fastapi.responses import StreamingResponse
+# @stub.function()
+# @web_endpoint()
+# def web(prompt: str):
+#     from fastapi.responses import StreamingResponse
 
-    return StreamingResponse(stream_chat(prompt), media_type="text/html")
+#     return StreamingResponse(stream_chat(prompt), media_type="text/html")
 
 
 # ## Try out the web endpoint
@@ -110,7 +119,7 @@ def web(prompt: str):
 # ## CLI interface
 #
 # Doing `modal run chatgpt_streaming.py --prompt="Generate a list of the world's most famous people"` also works, and uses the `local_entrypoint` defined below.
-
+default_user_input = "first principles"
 default_prompt = (
     "Generate a list of 20 great names for sentient cheesecakes that teach SQL"
 )
@@ -118,8 +127,21 @@ default_prompt = (
 
 @stub.local_entrypoint()
 def main(prompt: str = default_prompt):    
-    for part in stream_chat.call(prompt=prompt):
-        print(part, end="")
-    pinecone_init.call()
+    
+    store = pinecone_docsearch.call(default_user_input)
+    print("finished store", store)
+    # store = similarity_search(default_user_input, docsearch)
+    # print("finished similarity_search")
+    response = AI_response_messages.call(default_user_input, store)
+    print(response)
 
+    # for part in stream_chat.call(prompt=prompt):
+    #     print(part, end="")
 
+@stub.function()
+@web_endpoint(method="GET")
+def web(user_input: str):
+    store = pinecone_docsearch(user_input)
+    print("finished store", store)
+    response = AI_response_messages.call(user_input, store)
+    return response
